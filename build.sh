7c0f1d018fcc07f9a1a302a47d1bf309eed8423b
Muh Muhten <muh.muhten@gmail.com>
2015-10-11 11:02:05 -0400

    factor out version options

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 mkdir -p ~/.docker; echo "$hub_json" > ~/.docker/config.json
 
-docker run --rm vaca/builder -m http://alpine.gliderlabs.com/alpine -es | docker import - $hub_repo:edge
-docker push $hub_repo:edge
-
-docker tag $hub_repo:edge $hub_repo:latest
-docker push $hub_repo:latest
+for spec in versions/*; do
+  (. "$spec"
+  docker run vaca/builder -s $build_opts | docker import - build
+  for tag in $hub_tags; do
+    docker tag build $hub_repo:$tag
+    docker push $hub_repo:$tag
+  done)
+done
