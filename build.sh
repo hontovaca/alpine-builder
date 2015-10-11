39dba96c79cb3b5f9bdf758fc891726fab4ffdad
Muh Muhten <muh.muhten@gmail.com>
2015-10-11 10:14:07 -0400

    push directly to registry instead of going through build automation

--- build.sh
+++ build.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
-(umask 077; cat > ~/.ssh/id_ed25519) <<EOF
------BEGIN OPENSSH PRIVATE KEY-----
-$private_key
------END OPENSSH PRIVATE KEY-----
-EOF
-git config user.name none
-git config user.email none
-git remote set-url origin "`git config --get remote.origin.url | sed 's,https://\([^/]*\)/,git@\1:,'`"
+mkdir -p ~/.docker; echo "$hub_json" > ~/.docker/config.json
 
-for b in latest; do
-  git subtree split -P scratch -b "rootfs-$b" $TRAVIS_COMMIT
-  git checkout "rootfs-$b"
-  docker run --rm vaca/builder -m http://alpine.gliderlabs.com/alpine -es > rootfs.tar.gz
-  git add .
-  git commit -C $TRAVIS_COMMIT
-  git push -f origin "rootfs-$b"
-done
+docker run --rm vaca/builder -m http://alpine.gliderlabs.com/alpine -es | docker import - $hub_repo:edge
+docker push $hub_repo:edge
+
+docker tag $hub_repo:edge $hub_repo:latest
+docker push $hub_repo:latest
