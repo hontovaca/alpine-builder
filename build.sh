90405e838c57e674f3e5c367f8888ea9803a1592
Muh Muhten <muh.muhten@gmail.com>
2015-10-12 01:30:29 -0400

    actually fetch the versions specs to deploy

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 mkdir -p ~/.docker; echo "$hub_json" > ~/.docker/config.json
 
+git fetch --depth=1 origin versions:versions
+git clone -b versions . versions
+
 for spec in versions/*; do
   (. "$spec"
   docker run vaca/builder -s $build_opts | docker import - build
