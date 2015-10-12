cbccb4281f907f81a12cd4cadb256d2d5ac0660c
Muh Muhten <muh.muhten@gmail.com>
2015-10-12 01:53:31 -0400

    make the deploy script maybe slightly less ugly?

--- build.sh
+++ build.sh
@@ -1,14 +1,24 @@
-#!/bin/bash
-mkdir -p ~/.docker; echo "$hub_json" > ~/.docker/config.json
+#!/bin/bash -e
+
+if [ -n "$hub_json" ]; then
+  mkdir -p ~/.docker
+  echo "$hub_json" > ~/.docker/config.json
+fi
+
+if [ -z "$hub_repo" ]; then
+  # get something sluggy for the repo (don't rely on it)
+  hub_repo=$(cd ..; echo "${PWD##*/}")/${PWD##*/}
+fi
 
 git fetch --depth=1 origin versions:versions
 git clone -b versions . versions
 
 for spec in versions/*; do
   (. "$spec"
-  docker run vaca/builder -s $build_opts | docker import - build
-  for tag in $hub_tags; do
-    docker tag build $hub_repo:$tag
-    docker push $hub_repo:$tag
+  set -- $hub_tags
+  docker run vaca/builder -s $build_opts | docker import - "build:$1"
+  for tag; do
+    docker tag "build:$1" "$hub_repo:$tag"
+    docker push "$hub_repo:$tag"
   done)
 done
